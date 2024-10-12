71babd1b230e44a3ef89cf33a62af2646b9ade24
Mohammed Saleh <supertechman007@gmail.com>
2024-10-12 07:52:21 +0000

    creat new deconf file called sudo_deconfig that have some configertaioms that physwizz mentioned I hope it make the kernel bootable

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export ARCH=arm64
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y sudo_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j32
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
